50c3ed7c1f6690c8cfdf73ecd4f55b1b73cc297f
afknst <k.afknst@gmail.com>
2021-11-28 20:45:23 +0100

    0.1.7 update python wrapper

--- gxb2_battle.lua
+++ gxb2_battle.lua
@@ -1,26 +1,30 @@
-local teams = require("examples.teams")
+local function main()
+    local teams = require("examples.teams")
 
-local M = 5
-local N = #teams
-local SEEDS = get_seeds(M)
-local win_rates = "attacker,defender,win_rate\n"
-local log = "M = " .. M .. "\n\n"
+    local M = 5
+    local N = #teams
+    local SEEDS = get_seeds(M)
+    local win_rates = "attacker,defender,win_rate\n"
+    local log = "M = " .. M .. "\n\n"
 
-for i = 1, N do
-    for j = 1, N do
-        if i ~= j then
-            print(repeat_char("=", 30))
-            local msg = teams[i].name .. " vs " .. teams[j].name
-            local PARAMS = PvP_params(teams[i], teams[j])
-            local report = fight(PARAMS, SEEDS, true, msg)
-            win_rates = win_rates .. teams[i].id .. "," .. teams[j].id .. ","
-            win_rates = win_rates .. report.wins .. "\n"
-            log = log .. msg .. "\n" .. get_report(report)
-            print(repeat_char("=", 30) .. "\n")
+    for i = 1, N do
+        for j = 1, N do
+            if i ~= j then
+                print(repeat_char("=", 30))
+                local msg = teams[i].name .. " vs " .. teams[j].name
+                local PARAMS = PvP_params(teams[i], teams[j])
+                local report = fight(PARAMS, SEEDS, true, msg)
+                win_rates = win_rates .. teams[i].id .. "," .. teams[j].id .. ","
+                win_rates = win_rates .. report.wins .. "\n"
+                log = log .. msg .. "\n" .. get_report(report)
+                print(repeat_char("=", 30) .. "\n")
+            end
         end
+        save(win_rates, PREFIX .. "[win_rates].csv")
+        save(log, PREFIX .. "[log].log")
+        win_rates = ""
+        log = ""
     end
-    save(win_rates, PREFIX .. "[win_rates].csv")
-    save(log, PREFIX .. "[log].log")
-    win_rates = ""
-    log = ""
 end
+
+return main
--- main.py
+++ main.py
@@ -3,24 +3,61 @@ import sys
 import pexpect
 import numpy as np
 
-NUM = 3
-INITS = np.random.randint(2**23, size=NUM)
-CHILDS = [pexpect.spawn('lua -i -l preload') for _ in range(NUM)]
 
-for i in range(NUM):
-    CHILDS[i].logfile = sys.stdout.buffer
-for i in range(NUM):
-    CHILDS[i].expect('>')
+class GxB2:
+    def __init__(self, n=3, rng=None):
+        if rng is None:
+            rng = np.random.default_rng()
+        else:
+            rng = np.random.default_rng(rng)
+        self.n = n
+        self.rng = rng
+        self.fin = [False] * n
+        self.lua = [pexpect.spawn('lua -i -l preload') for _ in range(n)]
+        self.send([f'init({rng.integers(2**23)}, {_})' for _ in range(n)])
+        self.wait()
 
-for i in range(NUM):
-    CHILDS[i].sendline(f'init({INITS[i]}, {i})')
-for i in range(NUM):
-    CHILDS[i].expect('>')
+    def set_logfile(self, file):
+        for lua in self.lua:
+            lua.logfile = file
 
-for i in range(NUM):
-    CHILDS[i].sendline('require("gxb2_battle")')
-for i in range(NUM):
-    CHILDS[i].expect('>')
+    def check(self):
+        for i in range(self.n):
+            if not self.fin[i]:
+                if not self.lua[i].expect(['>', pexpect.TIMEOUT], timeout=0.1):
+                    self.fin[i] = True
 
-for i in range(NUM):
-    CHILDS[i].close()
+    def wait(self):
+        while not np.all(self.fin):
+            self.check()
+
+    def send(self, lua_code):
+        sent = [False] * self.n
+
+        if isinstance(lua_code, str):
+            lua_code = [lua_code] * self.n
+
+        while not np.all(sent):
+            for i in range(self.n):
+                if not sent[i]:
+                    if self.fin[i]:
+                        self.lua[i].sendline(lua_code[i])
+                        sent[i] = True
+                        self.fin[i] = False
+                    else:
+                        self.check()
+
+    def close(self):
+        self.wait()
+        for lua in self.lua:
+            lua.close()
+
+
+if __name__ == '__main__':
+    G = GxB2()
+    G.set_logfile(sys.stdout.buffer)
+    G.send('main = require("gxb2_battle")')
+    G.send('main()')
+    G.send('main()')
+    G.send('main()')
+    G.close()
